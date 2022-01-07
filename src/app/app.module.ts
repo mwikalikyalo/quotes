ebba4267d761663882283fa589b1bc8dcf626e4e
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-01-08 01:27:57 +0300

    Add the style links and bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { HomeComponent } from './home/home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CreateComponent } from './create/create.component';
 import { FormsModule} from '@angular/forms';
-import { FormComponent } from './form/form.component';
 import { QuotesComponent } from './quotes/quotes.component';
 
 @NgModule({
@@ -15,7 +14,6 @@ import { QuotesComponent } from './quotes/quotes.component';
     AppComponent,
     HomeComponent,
     CreateComponent,
-    FormComponent,
     QuotesComponent
   ],
   imports: [
