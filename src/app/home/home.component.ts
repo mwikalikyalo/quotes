720103f6243ca847d19dd377f2fd760540cd0cbf
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-01-09 02:16:29 +0300

    Add the links for the different sections of the page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { CreateComponent } from "@angular/create.component";
+import { getTokenSourceMapRange } from 'typescript';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  title= 'New Quote';
 
-  constructor() { }
+  constructor(private router: Router) {
+    goToPage(pageName: string):void{
+      this.router.navigate([`$[pageName]`]),
+    }
+   }
 
   ngOnInit(): void {
   }
---
+++ src/app/quote.ts
@@ -0,0 +1,6 @@
+export class quote {
+  quotes: string | undefined;
+  by: string | undefined; 
+  submitted: string | undefined;
+  category: string | undefined
+}
